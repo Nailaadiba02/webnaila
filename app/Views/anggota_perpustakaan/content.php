6afeae845146594281b8c2bdeb8409846d842c9c
Nailaadiba02 <137156825+Nailaadiba02@users.noreply.github.com>
2023-08-03 20:18:36 +0700

    first commit

--- app/Views/anggota_perpustakaan/content.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo $this->extend('template/index');
-echo $this->section('content');
-?>
-<p>content anggota_perpustakaan</p>
-<?php
-echo $this->endsection();
--- app/Views/buku/content.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo $this->extend('template/index');
-echo $this->section('content');
-?>
-<p>content buku</p>
-<?php
-echo $this->endsection();
